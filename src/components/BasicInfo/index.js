0f47e668ee273077575b7e18daa2d81904578f20
Gabriel Salinas <75760163+gass-git@users.noreply.github.com>
2021-11-08 11:10:14 +0100

    Improved components of row two

--- src/components/BasicInfo/index.js
+++ src/components/BasicInfo/index.js
@@ -5,18 +5,40 @@ const BasicInfo = () => {
   return (
   <section className="basic-info">
     <div className="border-img">
+      
       <div className="inner-container">
-        <div className="pic-name">
-          <div className="name">
-            GASS
-          </div>
-          <div className="pic">
+          <div className="pic-container">
             <img src="http://localhost:3000/images/avatar.png" alt="" />
           </div>
+        
+        <div className="username-wrapper">
+          <div className="username">
+            GASS
+          </div>
+          <div className="class">
+           💾 Developer
+          </div>
         </div>
+
         <div className="info">
-          asasd asdasd
+          <div>
+            <label>Profession</label>
+            <data>Engineer</data>
+          </div>
+          <div>
+            <label>Started Coding</label>
+            <data>2004</data>
+          </div>
+          <div>
+            <label>Turned Pro</label>
+            <data>2020</data>
+          </div>
+          <div>
+            <label>SO Points</label>
+            <data>2373</data>
+          </div>
         </div>
+      
       </div>
     </div>
   </section>
