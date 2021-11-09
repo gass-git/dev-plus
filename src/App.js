ac99655f36625e1f7121763581ddcc5147586b11
Gabriel Salinas <g.szada@gmail.com>
2021-11-09 20:43:23 +0100

    Activity component improved

--- src/App.js
+++ src/App.js
@@ -6,33 +6,49 @@ import BasicInfo from './components/basicInfo/index';
 import Projects from './components/projects/index';
 import Skills from './components/skills/index';
 import About from './components/about/index';
+import Activity from './components/activity/index';
 
-let answers_api =
-  "https://api.stackexchange.com/2.3/users/14895985/answers?order=desc&sort=activity&site=stackoverflow";
+let answers_api = "https://api.stackexchange.com/2.3/users/14895985/answers?order=desc&sort=activity&site=stackoverflow";
+let events_api = "https://api.github.com/users/gass-git/events/public";
+let repos_api = "https://api.github.com/users/gass-git/repos";
 
 const App = () => {
   var [selected, setSelected] = useState('about');
   var [answers, setAnswers] = useState([]);
+  var [gitEvents, setGitEvents] = useState([]);
+  var [repos, setRepos] = useState([]);
+
+  async function getGitEvents() {
+    var req = await fetch(events_api), 
+      respArray = await req.json(),
+      latestFive = respArray.slice(0,4); 
+    setGitEvents(latestFive);
+  }
+
+  async function getRepos() {
+    var req = await fetch(repos_api),
+      respArray = await req.json();
+    setRepos(respArray);
+  }
 
   async function get_SO_data(){
     
     // Get answers data
-    let resp = await fetch(answers_api);
-    let obj = await resp.json();
-    var answersArr = obj.items.slice(0, 5);
+    let req = await fetch(answers_api);
+    let resp = await req.json();
+    var answersArr = resp.items.slice(0, 4);
 
     console.log(answersArr)
 
-    let titles = [];
     let merged = [];
     // Get question title
     answersArr.forEach(async function(answer, index){
         
       var id = answer.question_id;
       let questions_api = `https://api.stackexchange.com/2.3/questions/${id}?order=desc&sort=activity&site=stackoverflow`;
-      let resp = await fetch(questions_api);
-      let obj = await resp.json();
-      let title = obj.items[0].title;
+      let req = await fetch(questions_api);
+      let resp = await resp.json();
+      let title = resp.items[0].title;
 
         merged.push({
          ...answersArr[index], 
@@ -45,7 +61,8 @@ const App = () => {
   };
 
   useEffect(() => {
-    get_SO_data();
+    getGitEvents();
+   // get_SO_data();
   }, []);
 
   return (
@@ -74,7 +91,7 @@ const App = () => {
               {selected === "about" ? <About /> : null}
               {selected === "skills" ? <Skills /> : null} 
               {selected === "projects" ? <Projects /> : null}
-           {/*   {selected === "activity" ? <Activity answers={answers} questions={questions} /> : null}  */}
+              {selected === "activity" ? <Activity answers={answers} gitEvents={gitEvents} /> : null}
             </div>
           </div>
         </div>
--- src/components/Activity/index.js
+++ src/components/Activity/index.js
@@ -1,51 +1,36 @@
-import React, {useState} from "react";
+import React, {Fragment, useState} from "react";
 import reactDom from "react-dom";
 import './activity.css';
 import Date from './childrens/date';
 
-const Activity = ({questions, answers}) => {
-
-  const Block = (question_id, answer_id, date) => {
-    let url = "https://stackoverflow.com/a/" + answer_id;
-
-    return (
-      <React.Fragment>
-      {/* <Date key={answer_id} entry={date} /> */}
-        <div className="block">
-          <a
-            className="cap"
-            key={question_id}
-            href={url}
-            target="_blank"
-            rel="noreferrer"
-            title="Go to answer"
-          >
-           
-          </a>
-        </div>
-      </React.Fragment>
-    );
-  };
+const Activity = ({answers, gitEvents}) => {
+  var space = <Fragment>&nbsp;&nbsp;&nbsp;</Fragment>;
 
   return (
     <section className="activity">
       <div className="title">Activity</div>
       <div className="content">
-        <div className="sub-title">
-          STACK OVERFLOW LATEST ANSWERS
-        </div>
-        <div className="container">
-          {answers.map((data, index) => {
-              return (
-                <Block
-                  key={data.answer_id}
-                  date={data.creation_date}
-                  question_id={data.question_id}
-                  answer_id={data.answer_id}
-                />
-              );
-            })}
-        </div>
+      <div className="sub-title">
+          GITHUB LATEST EVENTS
+      </div>
+      <div className="container">
+        {
+          gitEvents.map((data) => {
+            let repoName = data.repo.name;
+            repoName = repoName.slice(9);
+
+            return (
+              <div className='block'>
+                <div>{data.created_at}</div>
+                {space}
+                <div>Commit: {data.payload.commits[0].message} - </div>
+                {space}
+                <div>Repo: {repoName}</div>
+              </div>
+            )
+          })
+        }
+      </div>
       </div>
     </section>
   );
