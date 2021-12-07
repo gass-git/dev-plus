bf0ba018318965724e6454357a188db9cc8eea5d
Gabriel Salinas <g.szada@gmail.com>
2021-12-07 13:20:03 +0100

    MainMenu component code improved

--- src/components/mainMenu/index.js
+++ src/components/mainMenu/index.js
@@ -3,14 +3,12 @@ import './mainMenu.css';
 import useSound from "use-sound";
 import selectionSound from '../../assets/sounds/game-selection-sound.wav';
 
-const MainMenu = ({setSelected, isMenuActive}) => {
-  var [floorNumber, setFloorNumber] = useState(4);
-  const [playSwitchSound] = useSound(selectionSound, {volume: 1});
+export default function MainMenu ({setSelected, isMenuActive}) {
+  const [floorNumber, setFloorNumber] = useState(4),
+        [playSwitchSound] = useSound(selectionSound, {volume: 1});
 
   function select(entry){
     setSelected(entry);
-    
-    // Sound effect
     playSwitchSound();
     
     // Elevator
@@ -21,57 +19,40 @@ const MainMenu = ({setSelected, isMenuActive}) => {
   }
   
   return [
-  <section key="main-menu-identifier" className="main-menu">
-    <div className="border-img">
-      <div className="inner-container">
-        <div className="menu-wrapper">
-          <div 
-          className="option"
-          onClick={() => select('about')}
-          >
-            <div 
-            className="label">
-              About
+    <section key="main-menu-identifier" className="main-menu">
+      <div className="border-img">
+        <div className="inner-container">
+          <div className="menu-wrapper">
+            <div className="option" onClick={() => select('about')}>
+              <div 
+              className="label">
+                About
+              </div>
             </div>
-          </div>
-          <div 
-          className="option"
-          onClick={() => select('skills')}
-          >
-           
-            <div className="label">
-              Skills
+            <div className="option" onClick={() => select('skills')}>
+              <div className="label">
+                Skills
+              </div>
             </div>
-          </div>
-          <div 
-          className="option"
-          onClick={() => select('projects')}
-          >
-            
-            <div className="label">
-              Projects
+            <div className="option" onClick={() => select('projects')}>
+              <div className="label">
+                Projects
+              </div>
             </div>
-             
-          </div>
-          <div 
-          className="option"
-          onClick={() => select('activity')}
-          >
-            
-            <div className="label">
-              Activity
+            <div className="option"onClick={() => select('activity')}>
+              <div className="label">
+                Activity
+              </div>
             </div>
-          </div>
 
-          {/* ELEVATOR */}
-          <div className={isMenuActive ? `elevator floor-${floorNumber}` : null}>
-          </div>
+            {/* -- Elevator -- */}
+            <div className={isMenuActive ? `elevator floor-${floorNumber}` : null}>
+            </div>
+            {/* -------------- */}
 
+          </div>
         </div>
       </div>
-    </div>
-  </section>  
+    </section>  
   ]
 }
-
-export default MainMenu;
