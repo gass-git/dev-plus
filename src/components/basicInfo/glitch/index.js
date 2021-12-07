aaa8f401f18b83e16be11630c5a2c650149212dc
Gabriel Salinas <g.szada@gmail.com>
2021-12-07 13:12:35 +0100

    Improved BasicInfo component code

--- src/components/basicInfo/glitch/index.js
+++ src/components/basicInfo/glitch/index.js
@@ -2,9 +2,8 @@ import React from 'react';
 import './glitch.css';
 import avatar from  "../../../assets/images/avatar7.png"
 
-const Glitch = ({avatarGlitch}) => {
-
-    return (
+export default function Glitch ({avatarGlitch}) {
+    return [
         <section key="glitch-identifier" className={avatarGlitch ? "avatar-glitch effect" : "avatar-glitch"}>
             <div className="first"><img src={avatar} alt="" /></div>
             <span><img src={avatar} alt="" /></span>
@@ -12,7 +11,5 @@ const Glitch = ({avatarGlitch}) => {
             <span><img src={avatar} alt="" /></span>
             <span><img src={avatar} alt="" /></span>
         </section>
-    );
+    ]
 }
-
-export default Glitch;
--- src/components/basicInfo/index.js
+++ src/components/basicInfo/index.js
@@ -5,61 +5,59 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faArrowUp} from "@fortawesome/free-solid-svg-icons";
 import Glitch from './glitch';
 
-const BasicInfo = ({reputation, avatarGlitch}) => {
+export default function BasicInfo ({reputation, avatarGlitch}) {
   return [
-  <section key="basic-info-identifier" className="basic-info">
-    <div className="border-img">
-      
-      <div className="inner-container">
-      
-        <div className="avatar-wrapper">
-          <Glitch avatarGlitch={avatarGlitch}/>
-        </div>
-          
-        <ReactTooltip/>
+    <section key="basic-info-identifier" className="basic-info">
+      <div className="border-img">
+        
+        <div className="inner-container">
+        
+          <div className="avatar-wrapper">
+            <Glitch avatarGlitch={avatarGlitch}/>
+          </div>
+            
+          <ReactTooltip/>
 
-        {/* -- AVATAR AND NAME -- */}
-        <div className="username-wrapper">
-          <div className="username">
-            GASS
+          {/* -- Avatar & username -- */}
+          <div className="username-wrapper">
+            <div className="username">
+              GASS
+            </div>
+            <div className="sub-username">
+            Developer
+            </div>
           </div>
-          <div className="sub-username">
-           Developer
-          </div>
-        </div>
 
-        {/* -- INFO -- */}
-        <div className="info">
-          <div>
-            <label>Profession</label>
-            <data>Engineer</data>
+          {/* -- Profile summary -- */}
+          <div className="info">
+            <div>
+              <label>Profession</label>
+              <data>Engineer</data>
+            </div>
+            <div>
+              <label>Started Coding</label>
+              <data>2004</data>
+            </div>
+            <div>
+              <label>Turned Pro</label>
+              <data>2020</data>
+            </div>
+            <div>
+              <label data-tip="Stack Overflow reputation">SO Points</label>
+              <data>
+                <label data-tip="Total">{reputation.total} &nbsp;&nbsp;</label>
+                
+                <span style={{cursor:"default"}} data-tip="Month change">
+                  <FontAwesomeIcon icon={faArrowUp} className="icon shadow-04"/>
+                  &nbsp;&nbsp;
+                  {reputation.monthChange}
+                </span>
+              </data>
+            </div>
           </div>
-          <div>
-            <label>Started Coding</label>
-            <data>2004</data>
-          </div>
-          <div>
-            <label>Turned Pro</label>
-            <data>2020</data>
-          </div>
-          <div>
-            <label data-tip="Stack Overflow reputation">SO Points</label>
-            <data>
-              <label data-tip="Total">{reputation.total} &nbsp;&nbsp;</label>
-              
-              <span style={{cursor:"default"}} data-tip="Month change">
-                <FontAwesomeIcon icon={faArrowUp} className="icon shadow-04"/>
-                &nbsp;&nbsp;
-                {reputation.monthChange}
-              </span>
-            </data>
-          </div>
+        
         </div>
-      
       </div>
-    </div>
-  </section>
+    </section>
   ]
 }
-
-export default BasicInfo;
