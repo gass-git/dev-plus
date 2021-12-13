6d99634dd6ea9a671059c41ad41e1ff6f8345f9d
Gabriel Salinas <g.szada@gmail.com>
2021-12-13 12:01:10 +0100

    Changes to BasicInfo component

--- src/components/basicInfo/index.js
+++ src/components/basicInfo/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactTooltip from "react-tooltip";
 import './basicInfo.css';
+import avatar from  "../../assets/images/avatar7.png"
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faArrowUp} from "@fortawesome/free-solid-svg-icons";
-import avatar from  "../../assets/images/avatar7.png"
+
 
 export default function BasicInfo ({reputation}) {
   return [
