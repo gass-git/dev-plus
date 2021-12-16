785e926d447a3a9d8e03c8cd0bf021353293d2ba
Gabriel Salinas <g.szada@gmail.com>
2021-12-16 12:21:08 +0100

    Added sound effect to links component

--- src/components/links/index.js
+++ src/components/links/index.js
@@ -3,27 +3,30 @@ import "./links.css";
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faStackOverflow, faGithub, faLinkedin, faCodepen} from "@fortawesome/free-brands-svg-icons";
 import {faEnvelope, faSearch} from "@fortawesome/free-solid-svg-icons";
+import useSound from 'use-sound';
+import clickSound from '../../assets/sounds/link-click.mp3';
 
-
-export default function links(){
+export default function Links(){
+  const [playClickSound] = useSound(clickSound, {volume:0.8});
+  
   return[
     <main className="links">
       <div className="border-img">
         <div className="inner-container">
           <div className="icons-wrapper">
-            <a href="https://github.com/gass-git" target="_blank" rel="noreferrer">
+            <a onClick={() => playClickSound()} href="https://github.com/gass-git" target="_blank" rel="noreferrer">
               <FontAwesomeIcon icon={faGithub} style={{ fontSize:"35px",marginTop:"1px" }} className="fa-icon"/>
             </a>
-            <a href="https://stackoverflow.com/users/14895985/gass?tab=profile" target="_blank" rel="noreferrer">
+            <a onClick={() => playClickSound()} href="https://stackoverflow.com/users/14895985/gass?tab=profile" target="_blank" rel="noreferrer">
               <FontAwesomeIcon icon={faStackOverflow} style={{ fontSize:"36px" }} className="fa-icon"/>
             </a>
-            <a href="https://www.linkedin.com/in/gabriel-salinas-szada-7a188196" target="_blank" rel="noreferrer">
+            <a onClick={() => playClickSound()} href="https://www.linkedin.com/in/gabriel-salinas-szada-7a188196" target="_blank" rel="noreferrer">
               <FontAwesomeIcon icon={faLinkedin} style={{ fontSize:"36px", marginLeft:"6px" }}  className="fa-icon" />
             </a>
-            <a href="https://codesandbox.io/u/g.szada" target="_blank" rel="noreferrer">
+            <a onClick={() => playClickSound()} href="https://codesandbox.io/u/g.szada" target="_blank" rel="noreferrer">
               <FontAwesomeIcon icon={faCodepen} style={{ fontSize:"33px",marginTop:"2px" }} className="fa-icon" />
             </a>
-            <a href="https://codereview.stackexchange.com/users/239120/gass?tab=profile" target="_blank" rel="noreferrer">
+            <a onClick={() => playClickSound()} href="https://codereview.stackexchange.com/users/239120/gass?tab=profile" target="_blank" rel="noreferrer">
               <FontAwesomeIcon icon={faSearch} style={{ fontSize:"31px",marginTop:"2px" }} className="fa-icon" />
             </a>
           </div>
