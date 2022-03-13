3616f7d74507c0b9904e71b380efc80ceb38939a
Gabriel Salinas <g.szada@gmail.com>
2022-03-13 16:56:09 +0100

    Improved the organization of imports

--- src/App.js
+++ src/App.js
@@ -4,22 +4,18 @@ import 'aos/dist/aos.css'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import { appReducer, initialState, ACTIONS } from './stateCapsule'
 
-// APIs
-import { getAnswers, getReputation, getSkillScores } from './api/stackOverflow'
-import { getRepos, getGitEvents } from './api/github'
-import { processVisit, getUniqueVisits, getUserLocation } from './api/visits'
-import getWritings from './api/getWritings'
+// ---------------------------------------------------------------------------
+import {
+  getAnswers, getReputation, getSkillScores, getRepos,
+  getGitEvents, processVisit, getUniqueVisits, getUserLocation, getWritings
+} from './importsAPI'
 
-// Components
-import Activity from './components/activity/index'
-import ScrollDisplay from './components/scrollDisplay/index'
-import MainMenu from './components/mainMenu/index'
-import BasicInfo from './components/basicInfo/index'
-import Projects from './components/projects/index'
-import Skills from './components/skills/index'
-import About from './components/about/index'
-import FooterContent from './components/footerContent/index'
-import Links from './components/links/index'
+// ---------------------------------------------------------------------------
+import {
+  Activity, ScrollDisplay, MainMenu, BasicInfo, Projects,
+  Skills, About, FooterContent, Links
+} from './componentsImports'
+// ---------------------------------------------------------------------------
 
 export const AppContext = React.createContext(null)
 
