d0cbaa2620a8318383335d8c8a70d2e2f17fe180
Gabriel Salinas <g.szada@gmail.com>
2022-03-25 14:04:56 +0100

    TopNavBar component improved

--- src/components/topNavBar/index.js
+++ src/components/topNavBar/index.js
@@ -2,8 +2,64 @@ import React from 'react'
 import './topNavBar.css'
 import { faCaretLeft, faCaretRight } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import useWindowDimensions from '../../hooks/useWindowDimensions'
 
 export default function TopNavBar() {
+  const { width } = useWindowDimensions()
+
+  const LeftArrow = () => {
+    if (width >= 800) return (
+      <div
+        style={{
+          width: '10%',
+          textAlign: 'center',
+          marginTop: '-7px',
+          opacity: 0.2
+        }}
+      >
+        <FontAwesomeIcon icon={faCaretLeft} style={{ fontSize: '35px' }} />
+      </div>
+    )
+
+    if (width < 800) return (
+      <div
+        style={{
+          width: '10%',
+          textAlign: 'center',
+          marginTop: '-7px',
+          opacity: 0.9,
+          cursor: 'pointer'
+        }}
+      >
+        <FontAwesomeIcon icon={faCaretLeft} style={{ fontSize: '35px' }} />
+      </div>
+    )
+  }
+
+  const RightArrow = () => {
+    if (width >= 800) return (
+      <div style={{
+        width: '10%',
+        textAlign: 'center',
+        marginTop: '-7px',
+        opacity: 0.2
+      }}>
+        <FontAwesomeIcon icon={faCaretRight} style={{ fontSize: '35px' }} />
+      </div>
+    )
+
+    if (width < 800) return (
+      <div style={{
+        width: '10%',
+        textAlign: 'center',
+        marginTop: '-7px',
+        opacity: 0.9,
+        cursor: 'pointer'
+      }}>
+        <FontAwesomeIcon icon={faCaretRight} style={{ fontSize: '35px' }} />
+      </div>
+    )
+  }
 
   return (
     <section className='top-nav-bar'>
@@ -12,9 +68,7 @@ export default function TopNavBar() {
           <div className='wrapper'>
 
             {/* Left side container for left arrow */}
-            <div style={{ width: '10%', textAlign: 'center', marginTop: '-6px' }}>
-              <FontAwesomeIcon icon={faCaretLeft} style={{ fontSize: '35px' }} />
-            </div>
+            <LeftArrow />
 
             {/* Center flex parent container for links */}
             <div style={{ display: 'flex', width: '80%' }}>
@@ -37,9 +91,7 @@ export default function TopNavBar() {
             </div>
 
             {/* Right side container for right arrow */}
-            <div style={{ width: '10%', textAlign: 'center' }}>
-              <FontAwesomeIcon icon={faCaretRight} style={{ fontSize: '35px', marginTop: '-6px' }} />
-            </div>
+            <RightArrow />
 
           </div>
         </div>
--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ import './globalStyles/media.css';
 import './globalStyles/loader.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import TopNavBar from './components/topNavBar/index'
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      {/*<center><TopNavBar /></center>*/}
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
