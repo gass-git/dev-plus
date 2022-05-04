c1ffeadce799ee955c25a65b072038dc9e30fd01
Gabriel Salinas <75760163+gass-git@users.noreply.github.com>
2022-05-04 11:32:32 +0200

    getRepos fetch function improved

--- src/api/github.js
+++ src/api/github.js
@@ -42,38 +42,27 @@ function getGitEvents({ dispatch }) {
     })
 }
 function getRepos({ dispatch }) {
-  axios.get(repos_api)
-    .then((resp) => {
-      let newArray = [];
+  axios.get(repos_api).then((resp) => {
+      let newArray = []
 
       resp.data.forEach((repo) => {
-        newArray.push({
-          'name': repo.name,
-          'about': repo.description,
-          'url': repo.homepage,
-          'topics': repo.topics,
-          'created_at': repo.created_at
-        })
-
-      })
-
-      // Filter repos that don't have ABOUT SECTION and URL
-      let filteredArray = newArray.filter((repo) => {
-        if (repo.about !== null && repo.url !== "") {
-          return true
-        }
-        else {
-          return false // Remove repo
+        if(repo.description !== null && repo.homepage !== '' && !repo.fork){
+          newArray.push({
+            'name': repo.name,
+            'about': repo.description,
+            'url': repo.homepage,
+            'topics': repo.topics,
+            'created_at': repo.created_at
+          })
         }
       })
-
 
       // Sort repos from old to new
-      let sortedArray = filteredArray.sort((a, b) => {
+      newArray = newArray.sort((a, b) => {
         return (new Date(a.created_at) - new Date(b.created_at))
       })
 
-      dispatch({ type: SET_REPOS, repos: sortedArray })
+      dispatch({ type: SET_REPOS, repos: newArray })
     })
 }
 
